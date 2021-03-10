239f41e9c012f49be5cfbe6be6886b19e8e6749a
silanharmancii <silanharmanci.9@gmail.com>
2021-03-10 22:30:11 +0300

    10 mart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'], //birden fazla css eklenebilir.
 })
 export class AppComponent {
-  title = 'northwind';
+  title: string = 'northwind';
+  user: string = 'Şilan Harmancı';
+  //product:any={},any:her veri türü olabilir
+ 
 }
+
+//ts:datayı yönettiğin yer
+//angular'da her şey class
